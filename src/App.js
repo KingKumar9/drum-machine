d10908425bb2f92e351c88a717fed3cc2ddf04a9
Rajat Kumar <rajatk12199@gmail.com>
2021-12-03 13:19:57 +1100

    logic finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,116 @@
 import React from 'react';
+import $ from 'jquery'
+import pads from './pads.js';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      button: '',
+      loop: []
+    }
+    this.play = this.play.bind(this)
+    // this.loop = this.loop.bind(this)
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  handleKeyPress(e) {
+    let letter = e.key.toUpperCase()
+    switch(letter) {
+      case 'Q':
+        $('#' + letter + '-button').trigger('click')
+        break;
+      case 'W':
+        $('#' + letter + '-button').trigger('click')
+        break;
+      case 'E':
+        $('#' + letter + '-button').trigger('click')
+        break;
+      case 'A':
+        $('#' + letter + '-button').trigger('click')
+        break;
+      case 'S':
+        $('#' + letter + '-button').trigger('click')
+        break;
+      case 'D':
+        $('#' + letter + '-button').trigger('click')
+        break;
+      case 'Z':
+        $('#' + letter + '-button').trigger('click')
+        break;
+      case 'X':
+        $('#' + letter + '-button').trigger('click')
+        break;
+      case 'C':
+        $('#' + letter + '-button').trigger('click')
+        break;
+      default:
+        break;
+    }
+  }
+
+  play(e) {
+    let button = e.target.innerText
+    let audio = document.getElementById(button)
+    this.setState({
+      button: button
+    })
+    audio.volume = 1
+    audio.paused ? audio.play() : audio.currentTime = 0
+  }
+
+  /* LOOP FUNCTION FOR LOOP PADS IN DRUM MACHINE
+  loop(e) {
+    let loop = e.target.innerText
+    let audio = document.getElementById(loop)
+    audio.volume = 0.4
+    if (audio.loop) {
+      this.setState({
+        loop: this.state.loop.filter(i => i !== loop)
+      })
+      audio.loop = false
+      audio.load()
+      audio.currentTime = 0
+    } else {
+      this.setState({
+        loop: [...this.state.loop, loop]
+      })
+      audio.loop = true
+      audio.load()
+      audio.play()
+    }
+  }
+  */
+
+
   render() {
     return (
-      <div>
-        
+      <div className="container">
+        <div className="titleContainer">
+          <h1 className="title">drum machine</h1>
+        </div>
+        <div id="drum-machine">
+          <div id="display">
+            <div className="button-name">
+              { this.state.button }
+            </div>
+            <div className="loop-name">
+              { this.state.loop }
+            </div>
+          </div>
+          <div className="buttonContainer">
+            <div className="buttonSet">
+              {pads.map(i => {
+                return <button onClick={this.play} id={i.letter + '-button'} key={i.letter} className="drum-pad">{i.letter}<audio src={i.source} id={i.letter} className="clip" /></button>
+              })}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
---
+++ src/pads.js
@@ -0,0 +1,25 @@
+import sound_1 from './audio_files/sound_1.mp3'
+import sound_2 from './audio_files/sound_2.mp3'
+import sound_3 from './audio_files/sound_3.mp3'
+import sound_4 from './audio_files/sound_4.mp3'
+import sound_5 from './audio_files/sound_5.mp3'
+import sound_6 from './audio_files/sound_6.mp3'
+import sound_7 from './audio_files/sound_7.mp3'
+import sound_8 from './audio_files/sound_8.mp3'
+import sound_9 from './audio_files/sound_9.mp3'
+
+
+const pads = [
+    {  letter: 'Q', source: sound_1 },
+    {  letter: 'W', source: sound_2},
+    {  letter: 'E', source: sound_3 },
+    {  letter: 'A', source: sound_4 },
+    {  letter: 'S', source: sound_5 },
+    {  letter: 'D', source: sound_6 },
+    {  letter: 'Z', source: sound_7 },
+    {  letter: 'X', source: sound_8 },
+    {  letter: 'C', source: sound_9 }
+]
+
+
+export default pads
